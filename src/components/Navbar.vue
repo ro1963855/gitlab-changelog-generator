eeb9eff260378b9e7afe89c022cf0ed2bb5f970f
Dandy <dandy.chen@advantech.com.tw>
2018-03-29 10:29:40 +0800

    add Navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,14 @@
+<template id="app">
+    <b-navbar toggleable="md" type="dark" variant="dark">
+        <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+        <b-navbar-brand href="#">
+            Gitlab Changelog Generator 
+        </b-navbar-brand>
+    </b-navbar>
+</template>
+
+<script>
+export default {
+  name: '',
+};
+</script>
