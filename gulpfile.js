b718400be728d4f3232230946944053df9363336
Dandy <dandy.chen@advantech.com.tw>
2018-03-29 14:15:37 +0800

    add vscode miniify css watcher

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,35 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var minifyCSS = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
+var rename = require("gulp-rename");
 
-gulp.task('sass', function() {
+gulp.task('sass', function () {
     gulp.src('./src/assets/stylesheet/scss/**/*.scss')
         .pipe(sass())
         .pipe(gulp.dest('./src/assets/stylesheet/css/'))
 });
 
-gulp.task('default', ['sass'], function() {
+gulp.task('concat', function() {
+    return gulp.src('./src/assets/stylesheet/css/*.css')
+        .pipe(concat('all.css'))
+        .pipe(gulp.dest('./src/assets/stylesheet/css/'));
+});
+
+gulp.task('minify-css',['concat'], function() {
+    return gulp.src('./src/assets/stylesheet/css/all.css')
+      .pipe(minifyCSS({
+         keepBreaks: true,
+      }))
+      .pipe(rename(function(path) {
+        path.basename += ".min";
+        path.extname = ".css";
+      }))
+      .pipe(gulp.dest('./src/assets/stylesheet/css/'));
+});
+
+gulp.task('default', ['sass', 'minify-css'], function () {
     gulp.watch('./src/assets/stylesheet/scss/**/*.scss', ['sass']);
 });
