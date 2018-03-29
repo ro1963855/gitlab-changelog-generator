04aab1913096172901d204c48ed6482ff23c7bb0
Dandy <dandy.chen@advantech.com.tw>
2018-03-29 18:19:44 +0800

    add vscode typescript watcher

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,9 @@ var concat = require('gulp-concat');
 var minifyCSS = require('gulp-minify-css');
 var uglify = require('gulp-uglify');
 var rename = require("gulp-rename");
+var ts = require("gulp-typescript");
+var tsProject = ts.createProject("tsconfig.json");
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('sass', function () {
     gulp.src('./src/assets/stylesheet/scss/**/*.scss')
@@ -13,25 +16,34 @@ gulp.task('sass', function () {
         .pipe(gulp.dest('./src/assets/stylesheet/css/'))
 });
 
-gulp.task('concat', function() {
+gulp.task('concat', function () {
     return gulp.src('./src/assets/stylesheet/css/**/*.css')
         .pipe(concat('all.css'))
         .pipe(gulp.dest('./src/assets/stylesheet/build/'));
 });
 
-gulp.task('minify-css',['concat'], function() {
+gulp.task('minify-css', ['concat'], function () {
     return gulp.src('./src/assets/stylesheet/build/all.css')
-      .pipe(minifyCSS({
-         keepBreaks: true,
-      }))
-      .pipe(rename(function(path) {
-        path.basename += ".min";
-        path.extname = ".css";
-      }))
-      .pipe(gulp.dest('./src/assets/stylesheet/build/'));
+        .pipe(minifyCSS({
+            keepBreaks: true,
+        }))
+        .pipe(rename(function (path) {
+            path.basename += ".min";
+            path.extname = ".css";
+        }))
+        .pipe(gulp.dest('./src/assets/stylesheet/build/'));
 });
 
-gulp.task('default', ['sass', 'minify-css'], function () {
+gulp.task('typescript', function () {
+    return gulp.src('./src/assets/javascript/typescript/**/*.ts')
+        .pipe(sourcemaps.init())
+        .pipe(tsProject())
+        .js
+        .pipe(gulp.dest("./src/assets/javascript/js/"));
+});
+
+gulp.task('default', ['sass', 'minify-css', 'typescript'], function () {
     gulp.watch('./src/assets/stylesheet/scss/**/*.scss', ['sass']);
     gulp.watch('./src/assets/stylesheet/css/**/*.css', ['minify-css']);
+    gulp.watch('./src/assets/javascript/typescript/**/*.ts', ['typescript']);
 });
